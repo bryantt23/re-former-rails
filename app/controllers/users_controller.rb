1523e8416961d184eefa88d867a1e3abfa63d9f8
bryant tunbutr <bryantt23@users.noreply.github.com>
2025-11-15 02:37:27 +0800

    user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,17 @@ class UsersController < ApplicationController
 
 
   def create
-    @user =User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user =User.new(user_params)
 
     if @user.save
-      redirect_to new_users
+      redirect_to new_users_url
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  private
+  def user_params
+    params.expect(user: [ :username, :email, :password ])
   end
 end
